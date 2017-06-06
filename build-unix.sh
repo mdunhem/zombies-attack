e0dc72cf256d050a403f9ad58f5ddca37a6a4302
Mikael Dunhem <mikael.dunhem@gmail.com>
2017-06-06 16:39:54 -0400

    Fix typo in build-unix.sh

--- build-unix.sh
+++ build-unix.sh
@@ -2,11 +2,11 @@
 
 dir=`pwd`
 mkdir -p $dir/bin
-sml.bat build.sml
+sml build.sml
 
 cat > "$dir/bin/zombies" <<EOF
 #! /bin/sh
-exec sml.bat @SMLcmdname=\$0 @SMLload="$dir/bin/.heapimg" "\$@"
+exec sml @SMLcmdname=\$0 @SMLload="$dir/bin/.heapimg" "\$@"
 EOF
 
 chmod a+x "$dir/bin/zombies"
